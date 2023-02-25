f44a0cdcf018fa7114b3bbb33d44932cec783a03
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2023-02-25 15:59:48 +0100

    route params added

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum numquam dolorum autem aliquid similique, ducimus ipsa officia? Est eos magnam repellat eum, ad vero, ab numquam eaque, enim delectus ratione!</p>
+    </div>
+</template>
+
+<script setup>
+    const { id } = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
