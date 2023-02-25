422c17b950a4b4e5d55e480063b00a010a9a586b
YegorMykhalechko <yegor.mykhalechko@gmail.com>
2023-02-25 20:02:17 +0100

    added tailwind

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <header>
-      <nav>
-        <NuxtLink to="/">Project Name</NuxtLink>
-        <ul>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/" class="font-bold">Project Name</NuxtLink>
+        <ul class="flex gap-4">
           <li>
             <NuxtLink to="/">Home</NuxtLink>
           </li>
@@ -11,14 +11,13 @@
             <NuxtLink to="/about">About</NuxtLink>
           </li>
           <li>
-            <NuxtLink to="/products">Products</NuxtLink>
+            <NuxtLink to="/products" class="btn">Products</NuxtLink>
           </li>
         </ul>
-        <a href="/about">normal link</a>
       </nav>
     </header>
 
-    <div>
+    <div class="container mx-auto p-4">
       <slot />
     </div>
   </div>
